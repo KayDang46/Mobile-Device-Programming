3ec9e87711d5fee5b4587e7a5af327a25798e4f9
Khoi Dang <20041791.khoi@student.iuh.edu.vn>
2023-10-01 00:43:48 +0700

    Update

--- 06_DangTinhKhoi_Tuan03/Shopee/App.js
+++ 06_DangTinhKhoi_Tuan03/Shopee/App.js
@@ -3,6 +3,7 @@ import { Alert, Button, Pressable, StyleSheet, Text, View } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { useFonts } from 'expo-font';
 import Svg, { Circle } from 'react-native-svg';
+import { NavigationContainer } from '@react-navigation/native';
 
 let App = () => {
   let loginAlert = () => {
@@ -11,7 +12,7 @@ let App = () => {
         text: 'Cancel', onPress: () => console.log('Cancel Pressed'),
         style: 'cancel',
       },
-      { text: 'OK', onPress: () => console.log('OK Pressed'), },
+      { text: 'OK', onPress: () => console.log('OK Pressed')},
     ]);
   };
 
@@ -21,38 +22,44 @@ let App = () => {
         text: 'Cancel', onPress: () => console.log('Cancel Pressed'),
         style: 'cancel',
       },
-      { text: 'OK', onPress: () => console.log('OK Pressed') },
+      { text: 'OK', onPress: () => console.log('OK Pressed')},
     ]);
   };
 
   return (
-    <LinearGradient
-      style={styles.container}
-      colors={['rgba(0, 204, 249, 0)', 'rgba(0, 204, 249, 0.5804)', 'rgba(0, 204, 249, 0.6979)', 'rgba(0, 204, 249, 0.36)', 'rgba(0, 204, 249, 1)']}
-    >
-      <View style={styles.svg}>
-        <Svg xmlns="http://www.w3.org/2000/svg" width="140" height="140" viewBox="0 0 140 140" fill="none">
-          <Circle cx="70" cy="70" r="62.5" stroke="black" strokeWidth="15" />
-        </Svg>
-      </View>
+    <NavigationContainer>
+      <LinearGradient
+        style={styles.container}
+        colors={['rgba(199, 244, 246, 1)', 'rgba(209, 244, 246, 1)', 'rgba(229, 244, 245, 1)', 'rgba(0, 204, 249, 1)']}
+      >
+        <View style={styles.svg}>
+          <Svg xmlns="http://www.w3.org/2000/svg" width="140" height="140" viewBox="0 0 140 140" fill="none">
+            <Circle cx="70" cy="70" r="62.5" stroke="black" strokeWidth="15" />
+          </Svg>
+        </View>
 
-      <View style={styles.textContainer}>
-        <Text style={styles.mainText}>GROW{"\n"}YOUR BUSINESS</Text>
-        <Text style={styles.subText}>We will help you to grow your business using{"\n"}online server</Text>
-      </View>
+        <View style={styles.textContainer}>
+          <Text style={styles.mainText}>GROW{"\n"}YOUR BUSINESS</Text>
+          <Text style={styles.subText}>We will help you to grow your business using{"\n"}online server</Text>
+        </View>
 
-      <View style={styles.buttonContainer}>
-        <Pressable style={styles.buttonOutside} onPress={loginAlert}>
-          <Text style={styles.textInside}>LOGIN</Text>
-        </Pressable>
+        <View style={styles.buttonContainer}>
+          <Pressable style={styles.buttonOutside} onPress={loginAlert}>
+            <Text style={styles.textInside}>LOGIN</Text>
+          </Pressable>
+          
+          <Pressable style={styles.buttonOutside} onPress={signUpAlert}>
+            <Text style={styles.textInside}>SIGN UP</Text>
+          </Pressable>
+        </View>
 
-        <Pressable style={styles.buttonOutside} onPress={signUpAlert}>
-          <Text style={styles.textInside}>SIGN UP</Text>
-        </Pressable>
-      </View>
-    </LinearGradient>
+        <View style={styles.bottomText}>
+          <Text style={styles.textInside}>HOW WE WORK?</Text>
+        </View>
+      </LinearGradient>
+    </NavigationContainer>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -62,15 +69,15 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   svg: {
-    flex: 0.4,
     alignItems: 'center',
     justifyContent: 'center',
+    marginBottom: 66,
   },
   textContainer: {
-    flex: 0.3,
     alignItems: 'center',
     justifyContent: 'center',
     fontFamily: 'Roboto',
+    marginBottom: 50,
   },
   mainText: {
     fontWeight: 'bold',
@@ -78,6 +85,7 @@ const styles = StyleSheet.create({
     lineHeight: 29.3,
     textAlign: 'center',
     padding: 2,
+    marginBottom: 50,
   },
   subText: {
     fontWeight: 'bold',
@@ -85,13 +93,12 @@ const styles = StyleSheet.create({
     lineHeight: 17.58,
     textAlign: 'center',
     padding: 1.5,
-    marginTop: 62,
   },
   buttonContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     width: 320,
-    marginTop: 45,
+    marginBottom: 21,
   },
   buttonOutside: {
     backgroundColor: '#E3C000',
